06624efee4a3b7f703679a6ceb82d87a8d258ce8
Aakash Gupta <aakashtechworks@gmail.com>
2026-09-12 17:33:24 +0530

    feat: add transaction form validation completely

--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -5,6 +5,15 @@ import Button from '../common/Button'
 import { categories } from '../../constants/Categories'
 
 const TransactionForm = () => {
+    const initialFormdata = {
+        title: "",
+        amount: "",
+        type: "",
+        category: "",
+        date: "",
+    }
+
+
     const [formData, setFormData] = useState({
         title: "",
         amount: "",
@@ -12,6 +21,8 @@ const TransactionForm = () => {
         category: "",
         date: "",
     })
+
+    const [errors, setErrors] = useState({})
 
     const handleChange = (event) => {
         const {name , value} = event.target
@@ -24,20 +35,91 @@ const TransactionForm = () => {
                 category: "",
             }),
         }))
+
+        setErrors((prevErrors) => ({
+           ...prevErrors,
+           [name]: "",
+        }))
+    }
+
+    const validateForm = () => {
+        const newErrors = {}
+
+        if(!formData.title.trim()){
+            newErrors.title = "Title is required"
+        }
+
+        if(!formData.amount){
+            newErrors.amount = "Amount is required"
+        } else if (Number(formData.amount)<= 0) {
+            newErrors.amount = "Amount must be greater than 0"
+        }
+
+        if(!formData.type){
+            newErrors.type = "Please select a type"
+        }
+
+        if(!formData.category){
+            newErrors.category = "Please select a category"
+        }
+
+        if(!formData.date){
+            newErrors.date = "Please select a date"
+        }
+
+        setErrors(newErrors)
+
+        return Object.keys(newErrors).length === 0
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+
+      const isValid = validateForm()
+
+      if(!isValid) {
+        return
+      }
+
+      const transaction = {
+        id: crypto.randomUUID(),
+        ...formData,
+        amount: Number(formData.amount)
+      }
+
+      console.log(transaction)
+
+      setFormData(initialFormdata)
+      setErrors({})
     }
 
   return (
     <Card className='w-full max-w-2xl'>
         <h2 className='text-2xl font-bold text-gray-800'>Add Transaction</h2>
 
-        <form className='mt-6 flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className='mt-6 flex flex-col gap-4'>
             <Input name="title" value={formData.title} onChange={handleChange} placeholder="Enter transaction title" />
+            {
+                errors.title && (
+                    <p className='text-sm text-red-500'>{errors.title}</p>
+                )
+            }
             <Input type='number' value={formData.amount} onChange={handleChange} name="amount" placeholder="Enter amount" />
+            {
+                errors.amount && (
+                    <p className='text-sm text-red-500'>{errors.amount}</p>
+                )
+            }
             <select name='type' value={formData.type} onChange={handleChange} className='w-full rounded-lg border border-gray-300 bg-white px-4 py-3 outline-none transition focus:border-purple-500 focus:ring-2 focus:ring-purple-200'>
                 <option value="">Select type</option>
                 <option value="income">Income</option>
                 <option value="expense">Expense</option>
             </select>
+            {
+                errors.type && (
+                    <p className='text-sm text-red-500'>{errors.type}</p>
+                )
+            }
             <select name='category' value={formData.category} onChange={handleChange} disabled={!formData.type} className='w-full rounded-lg border border-gray-300 bg-white px-4 py-3 outline-none transition focus:border-purple-500 focus:ring-2 focus:ring-purple-200 disabled:cursor-not-allowed disabled:bg-gray-100'>
                 <option value="">Select category</option>
                 
@@ -48,7 +130,17 @@ const TransactionForm = () => {
                        ))
                 }
             </select>
-            <Input type='date' name="date" />
+            {
+                errors.category && (
+                    <p className='text-sm text-red-500'>{errors.category}</p>
+                )
+            }
+            <Input type='date' name="date" value={formData.date} onChange={handleChange}/>
+            {
+                errors.date && (
+                    <p className='text-sm text-red-500'>{errors.date}</p>
+                )
+            }
 
             <Button type='submit'>
                Add Transaction
